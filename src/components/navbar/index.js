fc6b56c67865afc726155f91fd6ab964f2e0471e
jacob nordan <jacob.nordan113322@gmail.com>
2022-04-23 20:31:28 -0400

    added simple navbar to start with

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,7 +1,50 @@
 function NavBar() {
   return (
-    <div>
-      <h1>First Class Painting</h1>
+    <div className="nav">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid">
+            <h4>First Class Painting</h4>
+            <button
+              class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="toggler-icon top-bar"></span>
+              <span class="toggler-icon middle-bar"></span>
+              <span class="toggler-icon bottom-bar"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a href="" class="nav-link active">
+                    nav_1
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="" class="nav-link active">
+                    nav_2
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="" class="nav-link active">
+                    nav_3
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="" class="nav-link active">
+                    nav_4
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
