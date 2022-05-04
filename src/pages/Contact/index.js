c31a66013027b006c83b7bbc2dce6c9c47e115e1
jacob nordan <jacob.nordan113322@gmail.com>
2022-05-04 17:27:39 -0400

    added a email form in contect section using emailjs

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,75 @@
+import emailjs from "emailjs-com";
+
 function Contact() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_r0p51mi",
+        "template_s3t8o7i",
+        e.target,
+        "BLuaANkIVDp-xGtFS"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
   return (
     <div className="contact">
       <h1>Contact Us!</h1>
+      <div className="container"></div>
+      <form onSubmit={sendEmail}>
+        <div className="row pt-5 mx-auto">
+          <div className="col-8 form-group mx-auto">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              name="name"
+            />
+          </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email Address"
+              name="email"
+            />
+          </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Subject"
+              name="subject"
+            />
+          </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+            <textarea
+              className="form-control"
+              id=""
+              cols="30"
+              rows="8"
+              placeholder="Your message..."
+              name="message"
+            />
+          </div>
+          <div className="col-8 pt-3 mx-auto">
+            <input
+              type="submit"
+              className="btn btn-info"
+              value="Send Message"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
