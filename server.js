a2ee7710953c4de0965659e35071e2e5237e314d
jacob nordan <jacob.nordan113322@gmail.com>
2022-05-18 13:19:39 -0400

    heroku push

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const WebpackDevServer = require("webpack-dev-server");
+const webpack = require("webpack");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,4 +17,17 @@ app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
+});
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  contentBase: "src/",
+  stats: { colors: true },
+}).listen(portNum, host, function (err, result) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Listening at http://" + host + ":" + portNum + "/");
 });
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var webpack = require("webpack");
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var CaseSensitivePathsPlugin = require("../index.js"); // use inside the npm package
+
+// Setup our plugins.
+var plugins = [
+  // attaches the webpack-generated JS to our main HTML file
+  new HtmlWebpackPlugin({ template: "./src/index.html" }),
+  // create global access to the NODE_ENV within our Webpacked code:
+  new webpack.DefinePlugin({
+    __ENV__: JSON.stringify(process.env.NODE_ENV),
+  }),
+  // Mac doesn't care about case, but linux servers do, so enforce...
+  new CaseSensitivePathsPlugin(),
+];
+
+module.exports = {
+  mode: "development",
+
+  // Bundle to our dist folder as a main.js file.
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
+  },
+
+  devtool: "source-map",
+
+  // Our master entry point.
+  entry: [
+    "webpack-dev-server/client?http://0.0.0.0:3000", // tells client where to get hot reloads
+    "webpack/hot/only-dev-server", // "only" prevents reload on syntax errors
+    "babel-polyfill", // for full ES6 compatibility on older devices
+    "./src/init.js",
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.json$/,
+        use: "json",
+      },
+    ],
+  },
+
+  plugins: plugins,
+};
